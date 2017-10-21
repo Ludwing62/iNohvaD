28b6cca38fe857d96008ccdc190ad5035c542262
ludwing <ludwingLGS62@gmail.com>
2017-10-21 11:32:55 -0500

    prueba

---
+++ prueba.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Bienvenido a iNohva</title>
+    </head>
+    <body>
+        <h2> bienvenido </h2>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
