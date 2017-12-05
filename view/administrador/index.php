0d6ee635a0a979c3f45244e829df445b8cc1c04b
Cesia\Benjamin <Ludwinglgs62@gmail.com>
2017-12-04 22:41:01 -0600

    cambio nuevo

--- view/administrador/index.php
+++ view/administrador/index.php
@@ -4,20 +4,13 @@ if(!isset($_SESSION["user_id"]) || $_SESSION["user_id"]==null){
   print "<script>alert(\"Acceso invalido!\");window.location='index.php';</script>";
 }
 ?>
-
 <!DOCTYPE HTML>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>Bienvenido Administrador</title>
-
         <!-- NavBar principal del administrador -->
         <?php include '../../lib/admin_navbar.php'; ?>
         <div id="container" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto"></div>
-
     </head>
-                    <body>
-
-                    </body>
-
 </html>
